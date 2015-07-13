cfa6f049f1ee85ee32640aeffcdbfb20171dacf9
YuriyValkiv <yurivalkiv@gmail.com>
2015-07-13 15:22:46 +0300

    changes in controller dir

--- src/main/java/com/mandy/astronomy/controller/RootController.java
+++ src/main/java/com/mandy/astronomy/controller/RootController.java
@@ -1,13 +1,33 @@
 package com.mandy.astronomy.controller;
 
+import com.mandy.astronomy.entity.Planets;
+import com.mandy.astronomy.service.PlanetsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class RootController {
 
+    @Autowired
+    private PlanetsService planetsService;
+
     @RequestMapping(value = "/")
     public String getIndex(){
         return "index";
+    }
+
+    @RequestMapping(value = "/about", method = RequestMethod.POST)
+    public @ResponseBody
+    Planets getAbout(
+            @RequestParam(value = "name", required = true) String name)
+    {
+        Planets planet = planetsService.getByName(name);
+        return planet;
     }
 }
