fc216e87fff9bf190f3c0cee1606e44b46ab1fd5
YuriyValkiv <yurivalkiv@gmail.com>
2015-07-27 18:09:20 +0300

    changes

--- src/main/java/com/mandy/astronomy/controller/RootController.java
+++ src/main/java/com/mandy/astronomy/controller/RootController.java
@@ -1,13 +1,12 @@
 package com.mandy.astronomy.controller;
 
-import com.mandy.astronomy.entity.Planets;
-import com.mandy.astronomy.service.PlanetsService;
+import com.mandy.astronomy.entity.Users;
+import com.mandy.astronomy.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -15,7 +14,7 @@ import java.util.List;
 public class RootController {
 
     @Autowired
-    private PlanetsService planetsService;
+    private UsersService usersService;
 
     @RequestMapping(value = "/")
     public String getIndex(){
@@ -27,17 +26,24 @@ public class RootController {
         return "linear_navigation";
     }
 
-//    @RequestMapping(value = "/about")
-//    public String getAbout(){
-//        return "about";
-//    }
-
-//    @RequestMapping(value = "/about", method = RequestMethod.GET)
-//    public @ResponseBody
-//    Planets getAbout(
-//            @RequestParam(value = "name", required = true) String name)
-//    {
-//        Planets planet = planetsService.getByName(name);
-//        return planet;
-//    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String checkUser(
+            @RequestParam("name") String name,
+            @RequestParam("pass") String pass)
+    {
+        List<Users> usersList = usersService.getAll();
+        int size = usersList.size();
+//        boolean check;
+        for (int i =0; i < size; i++){
+            Users user = usersList.get(i);
+            String uname = user.getName();
+            String upass = user.getPassword();
+            if (name.equals(uname) && pass.equals(upass)){
+                return "linear_navigation";
+            } else {
+                return "index";
+            }
+        }
+        return "stars";
+    }
 }
