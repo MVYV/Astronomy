26a17e36ed47a593932874ea43c42e3b67fceea3
YuriyValkiv <yurivalkiv@gmail.com>
2015-08-05 14:59:57 +0300

    delete CustomUser...

--- src/main/java/com/mandy/astronomy/service/impl/CustomUserDetailsService.java
+++
@@ -1,9 +0,0 @@
-//package com.mandy.astronomy.service.impl;
-//
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-///**
-// * Created by Yura on 28.07.2015.
-// */
-//public class CustomUserDetailsService implements UserDetailsService {
-//}
