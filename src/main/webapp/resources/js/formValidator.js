97f1398a3b1e6bd4b3919a8bb196d6a5126e96f0
MichaelValkiv <mishavalkiv@gmail.com>
2015-08-07 21:35:23 +0300

    d

--- src/main/webapp/resources/js/formValidator.js
+++ src/main/webapp/resources/js/formValidator.js
@@ -1,17 +1,27 @@
+function validationCorrect(vResult, vRules)
+{
+    vResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
+    vRules.style.color = '#008000';
+    return true;
+}
+
+function validationUnCorrect(vResult, vRules)
+{
+    vResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
+    vRules.style.color = '#ff0000';
+    return false;
+}
+
 function validateLogin(minLength, maxLength, inputField, validationResult, validationRules)
 {
     var regExpression = /^[A-z0-9_]+$/;
     if(inputField.value.length < minLength || inputField.value.length > maxLength || !inputField.value.match(regExpression))
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
 }
 
@@ -20,15 +30,11 @@ function validateEmail(inputField, validationResult, validationRules)
     var regExpression = /^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$/;
     if(!inputField.value.match(regExpression))
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
 }
 
@@ -37,15 +43,11 @@ function validateCountry(inputField, validationResult, validationRules)
     var regExpression = /^[a-zA-Z]+$/;
     if(!inputField.value.match(regExpression))
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
 }
 
@@ -54,15 +56,11 @@ function validateCity(inputField, validationResult, validationRules)
     var regExpression = /^[a-zA-Z]+$/;
     if(!inputField.value.match(regExpression))
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
 }
 
@@ -72,15 +70,11 @@ function validatePassword(inputField, validationResult, validationRules)
     var regExpression = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
     if(!inputField.value.match(regExpression))
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
 }
 
@@ -88,22 +82,18 @@ function validatePasswordConfirmation(inputFieldPass, inputFieldPassConf, valida
 {
     if(inputFieldPass.value == inputFieldPassConf.value)
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validYes.png")';
-        validationRules.style.color = '#008000';
-        return true;
+        validationCorrect(validationResult, validationRules)
     }
     else if(inputFieldPass.value != inputFieldPassConf.value)
     {
-        validationResult.style.backgroundImage = 'url("/resources/images/validNo.png")';
-        validationRules.style.color = '#ff0000';
-        return false;
+        validationUnCorrect(validationResult, validationRules)
     }
     else{}
 }
 
 function validateForm(form)
 {
-    if(validateLogin(2, 15,form["user_login"], form["validateLoginResult"], form["validateLoginRules"]) &&
+    if(validateLogin(2, 25, form["user_login"], form["validateLoginResult"], form["validateLoginRules"]) &&
        validateEmail(form["user_email"], form["validateEmailResult"], form["validateEmailRules"]) &&
        validateCountry(form["user_country"], form["validateCountryResult"], form["validateCountryRules"]) &&
        validateCity(form["user_city"], form["validateCityResult"], form["validateCityRules"]) &&
@@ -118,3 +108,26 @@ function validateForm(form)
     }
 }
 
+/*if(validateLogin(2, 25, form['user_login'], form['validateLoginResult'], form['validateLoginRules']) &&
+validateEmail(form['user_email'], form['validateEmailResult'], form['validateEmailRules']) &&
+validateCountry(form['user_country'], form['validateCountryResult'], form['validateCountryRules']) &&
+validateCity(form['user_city'], form['validateCityResult'], form['validateCityRules']) &&
+validatePassword(form['user_password'], form['validatePasswordResult'], form['validatePasswordRules']) &&
+validatePasswordConfirmation(form['user_password'], form['user_password_conf'], form['validatePasswordConfResult'], form['validatePasswordConfRules']))*/
+
+/*function testValidator(form)
+ {
+ if(document.getElementById('validateLoginRules').style.color == '#008000' &&
+ document.getElementById('validateEmailRules').style.color == '#008000' &&
+ document.getElementById('validateCountryRules').style.color == '#008000' &&
+ document.getElementById('validateCityRules').style.color == '#008000' &&
+ document.getElementById('validatePasswordRules').style.color == '#008000' &&
+ document.getElementById('validatePasswordConfRules').style.color == '#008000')
+ {
+ form.submit();
+ }
+ else
+ {
+ alert("FATAL ERROR!!!");
+ }
+ }*/
