29480dbb5d7b1627175cf66f41ee906fda83a5e0
YuriyValkiv <yurivalkiv@gmail.com>
2015-08-08 19:22:47 +0300

    0p0

--- src/main/java/com/mandy/astronomy/repository/SatellitesRepository.java
+++ src/main/java/com/mandy/astronomy/repository/SatellitesRepository.java
@@ -3,6 +3,9 @@ package com.mandy.astronomy.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.mandy.astronomy.entity.Satellites;
 
+import java.util.List;
+
 public interface SatellitesRepository extends JpaRepository<Satellites, Long> {
     public Satellites findByName(String name);
+    public List<Satellites> findByPlanet(String name);
 }
